a68623b1430d8a4aaf7fdf165731a576399f66d1
Rido <rido@airmail.cc>
2022-12-09 13:43:39 +1100

    slight changes to how colorschemes are setup

--- lua/core/colorschemes.lua
+++ lua/core/colorschemes.lua
@@ -1,24 +1,18 @@
--- Themes
+-- Theme settings
 local themes = {
-    pywal = function()
-        return 'pywal'
-    end,
     tokyonight = function()
         vim.g.tokyonight_style = 'night'
         vim.g.tokyonight_disable_italic_comment = '1'
-        return 'tokyonight'
     end,
     kanagawa = function()
         require('kanagawa').setup({ globalStatus = true })
-        return 'kanagawa'
-    end,
-    miramare = function()
-        return 'miramare'
     end,
 }
 
--- Selected theme
+-- Select theme
+-- pywal, tokyonight, kanagawa, miramare
 pcall(function()
-    local theme = themes.kanagawa()
-    vim.cmd('silent! colorscheme ' .. theme)
+    local theme_name = 'kanagawa'
+    pcall(themes[theme_name])
+    vim.cmd('silent! colorscheme ' .. theme_name)
 end)
