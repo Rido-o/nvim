900f3fcf31bef38bfb22e8ecb03c6853328bb840
Rido <rido@airmail.cc>
2023-01-21 14:49:41 +1100

    fixed gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -16,7 +16,7 @@ return {
             local gs = package.loaded.gitsigns
 
             local function map(mode, l, r, desc)
-                vim.keymap.set(mode, l, r, { buffer, desc })
+                vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
             end
 
             -- Actions
