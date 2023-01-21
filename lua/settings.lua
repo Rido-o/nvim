093d8d5254c19234995a10acccb2dd02046c6573
Rido <rido@airmail.cc>
2023-01-21 13:51:56 +1100

    made changed to settings.lua

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,45 +1,38 @@
--- Set options
-local function options(table)
-    for k, v in pairs(table) do
-        vim.opt[k] = v
-    end
-end
-
 -- Options
-options({
-    encoding = 'utf-8', -- The encoding displayed is UTF-8 by default
-    fileencoding = 'utf-8',
-    fileencodings = 'utf-8', -- Causes problems with fugitive
-    splitbelow = true, -- Split down instead of up
-    splitright = true, -- Split right instead of left
-    clipboard = 'unnamedplus', -- Copy and paste between everything
-    number = true, -- Shows the current line number
-    relativenumber = true, -- Enables the relative numberline
-    cursorline = true, -- Change the cursor in insert mode
-    linespace = 0, -- Set the space between lines
-    expandtab = true,
-    tabstop = 4,
-    softtabstop = 4, -- Set the width of tabs and space tabs
-    shiftwidth = 4, -- Change the number of space character for indentation
-    smartindent = true, -- Makes indenting smart
-    wrap = false, -- Display long lines as just one line
-    swapfile = false, -- Disable swap files
-    backup = false, -- Disable backup files
-    undofile = true, -- Allow undo files
-    ignorecase = true,
-    smartcase = true,
-    scrolloff = 5, -- How close the cursor should be before the page scrolls
-    timeoutlen = 250, -- Default is 1000, normally set to 500, used for which key
-    updatetime = 250, -- For vim signify ? not needed?
-    signcolumn = 'yes:1', -- Add permanant column for signs on the left
-    termguicolors = true, -- Required for some color schemes and colorizer
-    undodir = vim.fn.stdpath('cache') .. '/undo', -- Undo directory
-    completeopt = { 'menuone', 'noselect' }, -- Set completeopt to have a better completion experience
-    list = true,
-    listchars = { trail = '-', tab = '  ' }, -- 'trail:­,tab:  ,extends:»,precedes:«,nbsp:⣿,eol:¬'
-    laststatus = 3, -- Enables global statusline
-    guifont = { 'Hack Nerd Font Mono:h11' }, -- Font for gui nvim
-})
+local opt = vim.opt
+
+opt.encoding = 'utf-8' -- The encoding displayed is UTF-8 by default
+opt.fileencoding = 'utf-8'
+opt.fileencodings = 'utf-8' -- Causes problems with fugitive
+opt.splitbelow = true -- Split down
+opt.splitright = true -- Split right
+opt.clipboard = 'unnamedplus' -- Use system clipboard
+opt.number = true -- Print line number
+opt.relativenumber = true -- Enables the relative numberline
+opt.cursorline = true -- Change the cursor in insert mode
+opt.linespace = 0 -- Set the space between lines
+opt.expandtab = true -- Convert tabs to spaces
+opt.tabstop = 4
+opt.softtabstop = 4 -- Set the width of tabs and space tabs
+opt.shiftwidth = 4 -- Change the number of space character for indentation
+opt.smartindent = true -- Makes indenting smart
+opt.wrap = false -- Display long lines as just one line
+opt.swapfile = false -- Disable swap files
+opt.backup = false -- Disable backup files
+opt.undofile = true -- Allow undo files
+opt.ignorecase = true
+opt.smartcase = true
+opt.scrolloff = 5 -- How close the cursor should be before the page scrolls
+opt.timeoutlen = 250 -- Default is 1000, used for which key
+opt.updatetime = 250 -- For vim signify ? not needed?
+opt.signcolumn = 'yes:1' -- Add permanant column for signs on the left
+opt.termguicolors = true -- Required for some color schemes and colorizer
+opt.undodir = vim.fn.stdpath('cache') .. '/undo' -- Undo directory
+opt.completeopt = { 'menuone', 'noselect' } -- Set completeopt to have a better completion experience
+opt.list = true
+opt.listchars = { trail = '-', tab = '  ' } -- 'trail:­,tab:  ,extends:»,precedes:«,nbsp:⣿,eol:¬'
+opt.laststatus = 3 -- Enables global statusline
+opt.guifont = { 'Hack Nerd Font Mono:h11' } -- Font for gui nvim
 
 -- Set leader keys
 vim.g.mapleader = ' '
@@ -63,7 +56,7 @@ vim.api.nvim_create_autocmd('BufEnter', {
 
 -- Set shell to Windows PowerShell if on windows -- :help shell-powershell
 if vim.g.os == 'Windows' then
-    options({
+    opt({
         shell = 'pwsh.exe',
         shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command',
         shellxquote = '',
