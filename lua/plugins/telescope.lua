11ac019840b1f7620bdb77475bc2ee71335aa3c6
Rido <rido@airmail.cc>
2023-01-27 14:38:37 +1100

    beautify telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -32,13 +32,30 @@ return {
             mappings = {
                 i = {
                     ['<Tab>'] = function(...)
-                        return require('telescope.actions').move_selection_previous(...)
+                        return require('telescope.actions').move_selection_next(...)
                     end,
                     ['<S-Tab>'] = function(...)
-                        return require('telescope.actions').move_selection_next(...)
+                        return require('telescope.actions').move_selection_previous(...)
                     end,
                 },
             },
+            prompt_prefix = '   ',
+            selection_caret = '  ',
+            sorting_strategy = 'ascending',
+            layout_config = {
+                horizontal = {
+                    prompt_position = 'top',
+                    preview_width = 0.55,
+                    results_width = 0.8,
+                },
+                vertical = {
+                    mirror = false,
+                },
+                width = 0.87,
+                height = 0.80,
+                preview_cutoff = 120,
+            },
+            path_display = { 'truncate' }, -- ?
         },
         extensions = {
             fzf = {
@@ -53,5 +70,21 @@ return {
         require('telescope').setup(opts)
         pcall(require('telescope').load_extension, 'fzf')
         pcall(require('telescope').load_extension, 'projects')
+
+        local ts_sp1 = vim.api.nvim_get_hl_by_name('String', true).foreground
+        local ts_sp2 = vim.api.nvim_get_hl_by_name('Exception', true).foreground
+        local ts_light = vim.api.nvim_get_hl_by_name('ColorColumn', true).background
+        local ts_dark = vim.api.nvim_get_hl_by_name('NormalFloat', true).background
+
+        vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { fg = ts_light, bg = ts_light })
+        vim.api.nvim_set_hl(0, 'TelescopePromptNormal', { bg = ts_light })
+        vim.api.nvim_set_hl(0, 'TelescopeBorder', { fg = ts_dark, bg = ts_dark })
+        vim.api.nvim_set_hl(0, 'TelescopeNormal', { bg = ts_dark })
+
+        vim.api.nvim_set_hl(0, 'TelescopePromptPrefix', { fg = ts_sp2, bg = ts_light })
+        vim.api.nvim_set_hl(0, 'TelescopePreviewTitle', { fg = ts_light, bg = ts_sp1 })
+        vim.api.nvim_set_hl(0, 'TelescopePromptTitle', { fg = ts_light, bg = ts_sp2 })
+        vim.api.nvim_set_hl(0, 'TelescopeResultsTitle', { fg = ts_dark, bg = ts_dark })
+        vim.api.nvim_set_hl(0, 'TelescopeSelection', { bg = ts_light })
     end,
 }
