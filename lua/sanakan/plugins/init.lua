0015d248b61d986f851b776c580d84581a7a9aba
Rido <rido@airmail.cc>
2023-02-03 22:11:17 +1100

    added new potential plugins

--- lua/sanakan/plugins/init.lua
+++ lua/sanakan/plugins/init.lua
@@ -1,5 +1,5 @@
 return {
-    -- potential plugins { trouble.nvim, nvim-navic/barbecue, aerial.nvim, neogit, alpha-nvim, fidget.nvim, neorg/orgmode, lsp-zero, heirline.nvim, diffview.nvim, nvim-spectre, searchbox.nvim, statuscol.nvim, substitute.nvim, vim-wordmotion }
+    -- potential plugins { trouble.nvim, nvim-navic/barbecue, aerial.nvim, neogit, alpha-nvim, fidget.nvim, neorg/orgmode, lsp-zero, heirline.nvim, diffview.nvim, nvim-spectre, searchbox.nvim, statuscol.nvim, substitute.nvim, vim-wordmotion, }
 
     ---- UI ----
     {
