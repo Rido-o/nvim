a43c76c0fd3962591f1d1f8f3dea95bd3871c2c2
Rido <rido@airmail.cc>
2023-02-04 12:42:33 +1100

    replaced luasnip with nvim-snippy

--- lua/sanakan/plugins/cmp.lua
+++ lua/sanakan/plugins/cmp.lua
@@ -1,16 +1,26 @@
-return {
+local M = {}
+
+M[1] = {
+    -- Snippets
+    'dcampos/nvim-snippy',
+    dependencies = {
+        'dcampos/cmp-snippy',
+        'honza/vim-snippets',
+    },
+    ft = 'snippets',
+    cmd = { 'SnippyEdit', 'SnippyReload' },
+}
+
+M[2] = {
     'hrsh7th/nvim-cmp',
     dependencies = {
         'hrsh7th/cmp-nvim-lsp',
         'hrsh7th/cmp-buffer',
         'hrsh7th/cmp-path',
         'hrsh7th/cmp-cmdline',
-        -- Snippets
-        'L3MON4D3/LuaSnip',
-        'saadparwaiz1/cmp_luasnip',
-        'rafamadriz/friendly-snippets',
+        'dcampos/nvim-snippy',
     },
-    event = 'InsertEnter',
+    event = { 'InsertEnter', 'CmdlineEnter' },
     config = function()
         -- Functions used for tab mapping
         local has_words_before = function()
@@ -19,7 +29,7 @@ return {
         end
 
         local cmp = require('cmp')
-        local luasnip = require('luasnip')
+        local snippy = require('snippy')
 
         -- Lsp kind icons
         local kind_icons = require('sanakan.icons').kinds
@@ -28,11 +38,11 @@ return {
             snippet = {
                 -- REQUIRED - you must specify a snippet engine
                 expand = function(args)
-                    luasnip.lsp_expand(args.body) -- For `luasnip` users.
+                    require('snippy').expand_snippet(args.body) -- For `snippy` users.
                 end,
             },
             window = {
-                -- completion = cmp.config.window.bordered(),
+                completion = cmp.config.window.bordered(),
                 documentation = cmp.config.window.bordered(),
             },
             mapping = cmp.mapping.preset.insert({
@@ -45,8 +55,8 @@ return {
                 ['<Tab>'] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
-                    elseif luasnip.expand_or_jumpable() then
-                        luasnip.expand_or_jump()
+                    elseif snippy.can_expand_or_advance() then
+                        snippy.expand_or_advance()
                     elseif has_words_before() then
                         cmp.complete()
                     else
@@ -56,8 +66,8 @@ return {
                 ['<S-Tab>'] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_prev_item()
-                    elseif luasnip.jumpable(-1) then
-                        luasnip.jump(-1)
+                    elseif snippy.can_jump(-1) then
+                        snippy.previous()
                     else
                         fallback()
                     end
@@ -65,7 +75,7 @@ return {
             }),
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
+                { name = 'snippy' }, -- For snippy users.
                 { name = 'buffer' },
                 { name = 'path' },
                 { name = 'neorg' }, -- TODO make it require neorg to be installed
@@ -75,13 +85,13 @@ return {
                     -- Kind icons
                     vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
                     -- Source
-                    vim_item.menu = ({
-                        buffer = '[Buffer]',
-                        nvim_lsp = '[LSP]',
-                        luasnip = '[LuaSnip]',
-                        nvim_lua = '[Lua]',
-                        latex_symbols = '[LaTeX]',
-                    })[entry.source.name]
+                    -- vim_item.menu = ({
+                    --     buffer = '[Buffer]',
+                    --     nvim_lsp = '[LSP]',
+                    --     snippy = '[Snippy]',
+                    --     nvim_lua = '[Lua]',
+                    --     latex_symbols = '[LaTeX]',
+                    -- })[entry.source.name]
                     return vim_item
                 end,
             },
@@ -103,6 +113,7 @@ return {
                 { name = 'cmdline' },
             }),
         })
-        require('luasnip.loaders.from_vscode').lazy_load() -- TODO fix so it doesn't break initial install
     end,
 }
+
+return M
