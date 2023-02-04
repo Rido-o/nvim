e7bafb8e416cded0a5aa7f3fee441e873cb529ac
Rido <rido@airmail.cc>
2023-02-04 12:59:01 +1100

    cleanup

--- lua/sanakan/plugins/autopairs.lua
+++ lua/sanakan/plugins/autopairs.lua
@@ -11,8 +11,6 @@ return {
         require('nvim-autopairs').setup(opts)
 
         -- nvim-cmp integration, add () for lsp functions, test with require in lua
-        -- If you want insert `(` after select function or method item
-        -- TODO move to cmp config maybe
         local cmp_autopairs = require('nvim-autopairs.completion.cmp')
         local cmp_status, cmp = pcall(require, 'cmp')
         if cmp_status then
