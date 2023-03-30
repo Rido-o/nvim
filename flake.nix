7e962fac99d8887bfd3dbf257376b5aa93ab133f
Rido <rido@airmail.cc>
2023-03-30 14:31:38 +1100

    work in progress nix flake

--- flake.nix
+++ flake.nix
@@ -1,20 +1,38 @@
 {
+  # https://primamateria.github.io/blog/neovim-nix/#add-lua-script-config
   description = "My own Neovim flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     neovim = {
       url = "github:neovim/neovim/stable?dir=contrib";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, neovim }: {
+  outputs = { self, nixpkgs, neovim }:
+    let
+      overlayFlakeInputs = prev: final: {
+        neovim = neovim.packages.x86_64-linux.neovim;
+      };
 
-    packages.x86_64-linux.default = neovim.packages.x86_64-linux.neovim;
-    apps.x86_64-linux.default = {
-      type = "app";
-      program = "${neovim.packages.x86_64-linux.neovim}/bin/nvim";
+      overlayMyNeovim = prev: final: {
+        myNeovim = pkgs.wrapNeovim pkgs.neovim { } {
+          pkgs = final;
+        };
+      };
+
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [ overlayFlakeInputs overlayMyNeovim ];
+      };
+
+    in
+    {
+      packages.x86_64-linux.default = pkgs.myNeovim;
+      apps.x86_64-linux.default = {
+        type = "app";
+        program = "${pkgs.myNeovim}/bin/nvim";
+      };
     };
-  };
 }
