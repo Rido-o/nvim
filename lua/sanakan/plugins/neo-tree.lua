afce587de07caab5d65f5d97b6ff7e786d171774
Rido <rido@airmail.cc>
2023-07-18 15:15:46 +1000

    updated neotree to version 3

--- lua/sanakan/plugins/neo-tree.lua
+++ lua/sanakan/plugins/neo-tree.lua
@@ -17,7 +17,7 @@ end
 
 return {
     'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v2.x',
+    branch = 'v3.x',
     dependencies = {
         'nvim-lua/plenary.nvim',
         'kyazdani42/nvim-web-devicons',
