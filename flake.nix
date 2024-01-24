b8594619c9a46d54d129fa558edaf39ba56b1b02
Rido <rido@airmail.cc>
2024-01-24 14:14:58 +1100

    test 7

--- flake.nix
+++ flake.nix
@@ -22,32 +22,34 @@
         nvim =
           pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped
             (pkgs.neovimUtils.makeNeovimConfig
-              {
-                customRC = ''
-                  set runtimepath^=${./.}
-                '';
-                # source ${./.}/init.lua
-              }
-              # // {
-              #   wrapperArgs = [
-              #     "--prefix"
-              #     "PATH"
-              #     ":"
-              #     "${lib.makeBinPath [
-              #       pkgs.sumneko-lua-language-server
-              #       pkgs.gcc
-              #       pkgs.nil
-              #       pkgs.nodePackages.pyright
-              #       pkgs.statix
-              #       pkgs.nixpkgs-fmt
-              #       pkgs.stylua
-              #       pkgs.black
-              #       pkgs.gnumake
-              #       pkgs.ripgrep
-              #       pkgs.fd
-              #     ]}"
-              #   ];
+              # {
+              #   customRC = ''
+              #     set runtimepath^=${./.}
+              #     source ./init.lua
+              #   '';
               # }
+              # // 
+              {
+                wrapperArgs = [
+                  "--prefix"
+                  "PATH"
+                  ":"
+                  "${lib.makeBinPath [
+                    pkgs.sumneko-lua-language-server
+                    pkgs.gcc
+                    pkgs.nil
+                    pkgs.nodePackages.pyright
+                    pkgs.statix
+                    pkgs.nixpkgs-fmt
+                    pkgs.stylua
+                    pkgs.black
+                    pkgs.gnumake
+                    pkgs.ripgrep
+                    pkgs.fd
+                    pkgs.xclip
+                  ]}"
+                ];
+              }
             );
       in
       {
