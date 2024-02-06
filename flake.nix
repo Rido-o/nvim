525b075ace11feb78af23ecece22d4e9adab1503
Rido <rido@airmail.cc>
2024-02-06 22:31:48 +1100

    revert to the version that im sure works

--- flake.nix
+++ flake.nix
@@ -39,13 +39,13 @@
           ];
         });
     in
-    {
+    rec {
       packages = forAllSystems (pkgs: rec {
         neovim = nvim pkgs;
         default = neovim;
       });
       overlays = {
-        neovim = _: _: { neovim = systems; };
+        neovim = _: _: { neovim = packages.x86_64-linux.default; };
         default = self.overlays.neovim;
       };
     };
