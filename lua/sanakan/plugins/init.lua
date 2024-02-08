ebe2199fa8e84ee83e065ead43272edf30c11de6
Rido <rido@airmail.cc>
2024-02-08 18:09:53 +1100

    removed noice, too buggy

--- lua/sanakan/plugins/init.lua
+++ lua/sanakan/plugins/init.lua
@@ -189,26 +189,4 @@ return {
             })
         end,
     },
-    {
-        'folke/noice.nvim',
-        event = 'VeryLazy',
-        dependencies = {
-            'MunifTanjim/nui.nvim',
-            -- 'rcarriga/nvim-notify', -- optional
-        },
-        opts = {
-            lsp = {
-                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                override = {
-                    ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-                    ['vim.lsp.util.stylize_markdown'] = true,
-                    ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
-                },
-            },
-            -- you can enable a preset for easier configuration
-            presets = {
-                lsp_doc_border = true, -- add a border to hover docs and signature help
-            },
-        },
-    },
 }
